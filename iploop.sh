42339713be2f12999051db92da9a92aefb7874b7
EC2 Default User <ec2-user@ip-172-31-83-21.ec2.internal>
2023-06-18 04:00:04 +0000

    fixed -C option bug

--- iploop.sh
+++ iploop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-echo "test"
+echo "IpLooper Program -------------------"
 rm ./countries.txt
 rm ./results.txt
 rm ./hold.json
@@ -11,13 +11,11 @@ do
 	object=$(curl http://ip-api.com/json/"$i")
 	echo $object > hold.json
 	echo -e "$object\n" >> results.txt
-	country=$(jq -C .country hold.json | sed 's/"//g')
+	country=$(jq .country hold.json | sed 's/"//g')
 	echo -e "matched country: $country"
 	if [ "$country" == "United States" ]; then
-		echo "The IP address $i is in America" >> countries.txt
-		echo "" >> countries.txt
+		echo -e "The IP address $i is in America \n" >> countries.txt
 	else
-		echo "The IP address $i is not in America" >> countries.txt
-		echo "" >> countries.txt
+		echo -e "The IP address $i is not in America \n" >> countries.txt
 	fi
 done
